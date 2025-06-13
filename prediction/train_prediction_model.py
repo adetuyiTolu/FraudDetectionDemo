7409b0829ad465b60ce304ec12e58e86f6aae3af
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-06-13 15:00:47 -0400

    update prediction model

--- prediction/train_prediction_model.py
+++ prediction/train_prediction_model.py
@@ -1,22 +1,28 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from joblib import dump
 import random
 
 
 def is_risky(email, amount):
-    return email.endswith("tempmail.com") or amount > 5000
+    if email.endswith("tempmail.com") and amount > 5000:
+        return 2
+    elif email.endswith("tempemail.com") or amount > 5000:
+        return 1
+    else:
+        return 0
 
 
 data = []
 for _ in range(1000):
     amount = random.uniform(10, 10000)
     email = random.choice(["user@gmail.com", "fraud@tempmail.com", "legit@yahoo.com"])
-    label = int(is_risky(email, amount))
+    label = is_risky(email, amount)
     data.append((amount, int(email.endswith("tempmail.com")), label))
 
 df = pd.DataFrame(data, columns=["amount", "is_temp_email", "risk"])
+
+print(df)
 
 X = df[["amount", "is_temp_email"]]
 y = df["risk"]
