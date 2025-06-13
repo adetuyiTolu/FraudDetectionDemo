3eb4066d41ea96db99f949bc9a0376820f10f785
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-06-13 15:05:41 -0400

    update predictor

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -9,4 +9,9 @@ def predict(email, amount):
     is_temp_email = int(email.endswith("tempmail.com"))
     features = pd.DataFrame([{"amount": amount, "is_temp_email": is_temp_email}])
     prediction = model.predict(features)[0]
-    return "high" if prediction == 1 else "low"
+    if prediction == 2:
+        return "high"
+    elif prediction == 1:
+        return "medium"
+    else:
+        return "low"
